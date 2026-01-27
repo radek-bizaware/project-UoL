1c760f94b5b06b669d0a3830b3e4f731e5f37808
BizAware <letstalk@bizaware.dev>
2026-01-27 19:58:47 +0000

    initial setup cd

--- Pokemon_Guessing_Game/app.py
+++ Pokemon_Guessing_Game/app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pypokedex
+import pandas as pd
 st.set_page_config(page_title="Pokemon Guessing Game")
 st.title("Welcome to the most amazing Pokemon Guessing Game")
-st.write("streamlit is working")
+st.write("The rules are simple: guess as many as you can!")
 
 Guessed_Pokemons = []
-Leaderboard = []
+Leaderboard = pd.read_csv('Scoreboard.csv')
+top20 = Leaderboard.sort_values('Score', ascending=False).head(20)
+st.dataframe(top20)
 Player_Name = name = st.text_input("Name", placeholder="Type your name here")
