c0dbf27b1c566d25109161e4b3a560f4b3333a7a
BizAware <letstalk@bizaware.dev>
2026-02-03 18:08:54 +0000

    2nd section definition

--- Pokemon_Guessing_Game/app.py
+++ Pokemon_Guessing_Game/app.py
@@ -8,5 +8,13 @@ st.write("The rules are simple: guess as many as you can!")
 Guessed_Pokemons = []
 Leaderboard = pd.read_csv('Scoreboard.csv')
 top20 = Leaderboard.sort_values('Score', ascending=False).head(20)
-st.dataframe(top20)
+st.table(top20)
 Player_Name = name = st.text_input("Name", placeholder="Type your name here")
+if "show_section" not in st.session_state:
+    st.session_state["show_section"] = False
+
+if st.button("Start New Game"):
+    st.session_state["show_section"] = True
+
+if st.session_state["show_section"]:
+    st.subheader("The Game Starts Now")
