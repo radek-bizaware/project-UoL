281900edd1579175ff223b376b86a073375b017c
BizAware <letstalk@bizaware.dev>
2026-02-03 18:35:53 +0000

    2nd section definition

--- Pokemon_Guessing_Game/app.py
+++ Pokemon_Guessing_Game/app.py
@@ -5,18 +5,18 @@ st.set_page_config(page_title="Pokemon Guessing Game")
 st.title("Welcome to the most amazing Pokemon Guessing Game")
 st.write("The rules are simple: guess as many as you can!")
 
-Guessed_Pokemons = []
+Game_Guesses = st.session_state["Guessed_Pokemons"]
 Leaderboard = pd.read_csv('Scoreboard.csv')
 top20 = Leaderboard.sort_values('Score', ascending=False).head(20)
 st.table(top20)
 Player_Name = name = st.text_input("Name", placeholder="Type your name here")
-if "show_section" not in st.session_state:
+'''if "show_section" not in st.session_state:
     st.session_state["show_section"] = False
 
 if st.button("Start New Game"):
     st.session_state["show_section"] = True
 
-
+'''
 # ---- FORM ----
 with st.form("guess_form", clear_on_submit=True):
     guess = st.text_input("Can you guess any Pokémon?")
@@ -25,6 +25,4 @@ with st.form("guess_form", clear_on_submit=True):
     if submitted and guess:
         st.session_state.Guessed_Pokemons.append(guess.strip().lower())
 
-# ---- DISPLAY LIST ----
-st.subheader("Your guesses")
-st.write(st.session_state.Guessed_Pokemons)
+
